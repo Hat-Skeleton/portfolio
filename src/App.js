00c42a639b56e304de66604d69e06d4d048c5d39
Cody <89228187+Hat-Skeleton@users.noreply.github.com>
2023-05-03 17:48:33 +1200

    favicon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     location: "Invercargill, NZ",
     email: "codydpc@gmail.com",
     availability: "Open for work",
-    brand: "REMOVE",
+    brand:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus ultricies turpis. Maecenas varius, est a semper fringilla, diam arcu malesuada nibh, et pellentesque lacus leo aliquet leo. Nam malesuada felis dolor, sit amet tristique dolor gravida id. Sed sapien orci, ullamcorper et eros at, iaculis lacinia nibh. Suspendisse ultrices faucibus laoreet. Duis placerat purus sed consequat mattis. Nunc eleifend sagittis lorem eget hendrerit. Vivamus facilisis est posuere turpis bibendum, in eleifend purus gravida. Sed non faucibus elit, non accumsan leo. Mauris porta elit ut velit aliquet feugiat ut eu dui. Duis ut tellus felis.",
   };
 
   return (
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -51,9 +51,7 @@ const AboutMe = ({ name, email, location, availability, brand }) => {
         >
           <div className="contentContainer">
             <h4>Nice to meet you</h4>
-            <h5>
-              Frontend Web Developer who creates amazing digital experiences!
-            </h5>
+            <h5>Te Pūkenga third year student</h5>
             <div className="contentDescription">
               <p>{brand}</p>
             </div>
